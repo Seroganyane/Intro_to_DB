0e2f8cc005a408142227c04192d12e1cfbc55d7c
Ponagalo <seroganyanemathaba@gmail.com>
2025-10-18 14:12:57 +0200

    Updated Successfully

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,7 +29,7 @@ CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DECIMAL(10, 2) NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 
