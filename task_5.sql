c3f289b5922f0c3f7aa4a6d9e9025808efe1a8fb
Ponagalo <seroganyanemathaba@gmail.com>
2025-10-19 23:02:36 +0200

    Fixed problem

--- task_5.sql
+++ task_5.sql
@@ -6,4 +6,4 @@ CREATE TABLE customer (
     address TEXT
 );
 INSERT INTO customer(customer_id, customer_name, email, address) VALUES
-(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave");
+(1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
