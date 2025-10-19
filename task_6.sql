73aed8abc40fe5556d39c83c002fdeae6570e4d9
Ponagalo <seroganyanemathaba@gmail.com>
2025-10-19 23:09:36 +0200

    Adding multiple details

---
+++ task_6.sql
@@ -0,0 +1,11 @@
+USE alx_book_store;
+CREATE TABLE customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+INSERT INTO customer(customer_id, customer_name, email, address) VALUES
+(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+(3, "Obed Ehoneah","eobed@sandtech.com","125 Happiness Ave."),
+(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
